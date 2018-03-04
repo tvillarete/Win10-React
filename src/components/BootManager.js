143f060cfd873a579813a049648beb3c7cccef61
Tanner Villarete <tvillarete@gmail.com>
2018-03-04 11:00:15 -0800

    Boot Selection: Add new screen to select OS

--- src/components/BootManager.js
+++ src/components/BootManager.js
@@ -6,17 +6,25 @@ class BootManager extends Component {
    constructor(props) {
       super(props);
       this.handleOSChange = this.handleOSChange.bind(this);
+      this.getCurrentView = this.getCurrentView.bind(this);
    }
 
    handleOSChange(options) {
-      this.setState((state) => {
-         if (state.activeOS === 'MacOS') {
-            state.activeOS = 'Windows10';
-         } else {
-            state.activeOS = 'MacOS';
-         }
-         return state;
-      });
+      if (options === 'MacOS' || options === 'Windows10') {
+         this.setState(state=> {
+            state.activeOS = options;
+            return state;
+         })
+      } else {
+         this.setState((state) => {
+            if (state.activeOS === 'MacOS') {
+               state.activeOS = 'Windows10';
+            } else {
+               state.activeOS = 'MacOS';
+            }
+            return state;
+         });
+      }
    }
 
    componentWillMount() {
@@ -27,13 +35,90 @@ class BootManager extends Component {
             Windows10: <Windows10
                         onOSChange={this.handleOSChange}/>
          },
-         activeOS: 'MacOS',
-      })
+         activeOS: '',
+      });
+   }
+
+   getCurrentView() {
+      if (!this.state.activeOS) {
+         return <BootSelector
+          list={this.state.list}
+          onEvent={this.handleOSChange}/>
+      } else {
+         return this.state.list[this.state.activeOS];
+      }
    }
 
    render() {
       return (
-         this.state.list[this.state.activeOS]
+         this.getCurrentView()
+      );
+   }
+}
+
+class BootSelector extends Component {
+   constructor(props) {
+      super(props);
+      this.handleEvent = this.handleEvent.bind(this);
+      this.handleKeydown = this.handleKeydown.bind(this);
+      this.state = {
+         cursor: 0,
+         selected: 'Windows10'
+      }
+   }
+
+   handleEvent(text) {
+      this.setState({
+         selected: text
+      });
+      this.props.onEvent(text)
+   }
+
+   handleKeydown(e) {
+      const {cursor, selected} = this.state;
+
+      if (e.key === 39 && cursor < this.state.list.length-1) {
+         this.setState(prevState => ({
+            cursor: prevState.cursor + 1
+         }))
+      } else if (e.key === 37 && cursor > 0) {
+         this.setState(prevState => ({
+            cursor: prevState.cursor - 1
+         }))
+      }
+      console.log(cursor);
+   }
+
+   render() {
+      return (
+         <div className="boot-manager" onKeyPress={this.handleKeydown}>
+            <h1>Select OS</h1>
+            <div className="os-container">
+               <BootItem
+                text="Windows10"
+                selected={this.state.selected === "Windows10"}
+                img="files/images/icons/windows_black.svg"
+                onEvent={this.handleEvent}/>
+               <BootItem
+                text="MacOS"
+                selected={this.state.selected === 'MacOS'}
+                img="files/images/icons/apple.svg"
+                onEvent={this.handleEvent}/>
+            </div>
+         </div>
+      );
+   }
+}
+
+class BootItem extends Component {
+   render() {
+      return (
+         <div
+          className={`os-item ${this.props.selected ? 'selected' : ''}`}
+          onClick={()=>{this.props.onEvent(this.props.text)}}>
+            <img src={this.props.img} />
+            <h3>{this.props.text}</h3>
+         </div>
       );
    }
 }
